f0d7eb4679e5ee69faad66529a29b28882d0f043
Paweł Awramiuk <awramiukofficial@gmail.com>
2021-05-16 20:15:10 +0200

    Wstępne klasy pod bazę przepisów

---
+++ Przepisy_kulinarne_projekt/Przepisy/Przepis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Przepisy_kulinarne_projekt.Przepisy
+{
+    public class Przepis
+    {
+        public string Nazwa { get; set; }
+        public string ListaSkładników { get; set; }
+        public string OpisWykonania { get; set; }
+        public DateTime DataPublikacji { get; set; }
+    }
+}
---
+++ Przepisy_kulinarne_projekt/Przepisy/PrzepisyDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Przepisy_kulinarne_projekt.Przepisy
+{
+    public class PrzepisyDBContext : DbContext
+    {
+        public PrzepisyDBContext(DbContextOptions options) : base(options)
+        {}
+        public DbSet<Przepis> Przepisy { get; set; }
+    }
+}
