00b6fca85822ce6ca7a5c4c54114c57a8926ff65
Paweł Awramiuk <awramiukofficial@gmail.com>
2021-06-10 20:24:11 +0200

    Praktycznie gotowy Profile Page

---
+++ Przepisy_kulinarne_projekt/Pages/RecipeDetails.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Przepisy_kulinarne_projekt.Pages.Shared
+{
+    public class RecipeDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
