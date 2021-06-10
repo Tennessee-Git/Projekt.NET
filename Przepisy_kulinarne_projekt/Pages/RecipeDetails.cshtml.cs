4f72960a1323b91c7500f18b9888aae71d38c426
Paweł Awramiuk <awramiukofficial@gmail.com>
2021-06-10 20:53:38 +0200

    Poprawiony redirect w LoginPartial

--- Przepisy_kulinarne_projekt/Pages/RecipeDetails.cshtml.cs
+++ Przepisy_kulinarne_projekt/Pages/RecipeDetails.cshtml.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Przepisy_kulinarne_projekt.Models;
 
 namespace Przepisy_kulinarne_projekt.Pages.Shared
 {
     public class RecipeDetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly Przepisy_kulinarne_projekt.Data.ApplicationDbContext _context;
+
+        public RecipeDetailsModel(Przepisy_kulinarne_projekt.Data.ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public Recipe Recipe { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Recipe = await _context.Recipes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Recipe == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
