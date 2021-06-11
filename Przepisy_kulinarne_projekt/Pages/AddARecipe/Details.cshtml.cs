d63461d2db247bdf2a8479296e830837e045f440
Paweł Awramiuk <awramiukofficial@gmail.com>
2021-06-11 15:05:45 +0200

    Część szczegółów przepisu

--- Przepisy_kulinarne_projekt/Pages/AddARecipe/Details.cshtml.cs
+++ Przepisy_kulinarne_projekt/Pages/AddARecipe/Details.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -34,6 +35,7 @@ namespace Przepisy_kulinarne_projekt.Pages.AddARecipe
             {
                 return NotFound();
             }
+            
             return Page();
         }
     }
