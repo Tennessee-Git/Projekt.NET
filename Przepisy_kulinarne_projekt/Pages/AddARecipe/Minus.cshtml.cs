c8d5cb723fdfc92195f02b039e2df99409db837b
gromorth <filip.roszkowski@wp.pl>
2021-06-16 03:24:23 +0200

    kaszanka w kosmosie

---
+++ Przepisy_kulinarne_projekt/Pages/AddARecipe/Minus.cshtml.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Przepisy_kulinarne_projekt.Data;
+using Przepisy_kulinarne_projekt.Models;
+
+namespace Przepisy_kulinarne_projekt.Pages.AddARecipe
+{
+    public class MinusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        private readonly Przepisy_kulinarne_projekt.Data.ApplicationDbContext _context;
+
+        public MinusModel(Przepisy_kulinarne_projekt.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Recipe Recipe { get; set; }
+
+        public async Task<IActionResult> OnPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Recipe = await _context.Recipes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Recipe == null)
+            {
+                return NotFound();
+            }
+
+            Recipe.Rating--;
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+
+        }
+    }
+}
